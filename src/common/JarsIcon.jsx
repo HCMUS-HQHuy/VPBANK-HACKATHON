cd9dbba4ab0ea5a266c375a7c9be274c7193aca8
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-07-13 01:56:52 +0700

    refactor: change JarsIcon from array to object for improved structure and access

--- src/common/JarsIcon.jsx
+++ src/common/JarsIcon.jsx
@@ -1,12 +1,49 @@
 import { faHouseChimney, faPiggyBank, faMartiniGlassCitrus, faGraduationCap, faHandHoldingDollar, faGift } from '@fortawesome/free-solid-svg-icons'
 
-const JarsIcon = [
-	{ label: 'Necessities',	percent: '55%', icon: faHouseChimney,		colorLight: 'primary-light',color: 'primary' },
-	{ label: 'Savings',		percent: '10%', icon: faPiggyBank, 			colorLight: 'green-light', 	color: 'green' },
-	{ label: 'Fun',			percent: '10%', icon: faMartiniGlassCitrus, colorLight: 'yellow-light', color: 'yellow' },
-	{ label: 'Education',	percent: '10%', icon: faGraduationCap, 		colorLight: 'blue-light', 	color: 'blue' },
-	{ label: 'Invest',		percent: '10%', icon: faHandHoldingDollar, 	colorLight: 'danger-light', color: 'danger' },
-	{ label: 'Give',		percent: '5%', 	icon: faGift,				colorLight: 'pink-light', 	color: 'pink' },
-];
+const JarsIcon = {
+  Necessities: {
+    label: 'Necessities',
+    percent: '55%',
+    icon: faHouseChimney,
+    colorLight: 'primary-light',
+    color: 'primary'
+  },
+  Savings: {
+    label: 'Savings',
+    percent: '10%',
+    icon: faPiggyBank,
+    colorLight: 'green-light',
+    color: 'green'
+  },
+  Fun: {
+    label: 'Fun',
+    percent: '10%',
+    icon: faMartiniGlassCitrus,
+    colorLight: 'yellow-light',
+    color: 'yellow'
+  },
+  Education: {
+    label: 'Education',
+    percent: '10%',
+    icon: faGraduationCap,
+    colorLight: 'blue-light',
+    color: 'blue'
+  },
+  Invest: {
+    label: 'Invest',
+    percent: '10%',
+    icon: faHandHoldingDollar,
+    colorLight: 'danger-light',
+    color: 'danger'
+  },
+  Give: {
+    label: 'Give',
+    percent: '5%',
+    icon: faGift,
+    colorLight: 'pink-light',
+    color: 'pink'
+  }
+};
+
 
 export default JarsIcon;
